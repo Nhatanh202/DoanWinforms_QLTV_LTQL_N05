998aa27792f7b5e4c130722df1b9f90a6c09579e
Orbexis <nnhatanh487@gmail.com>
2026-03-28 20:30:49 +0700

    update cau truc project

--- QLTV_LTQL_N05/Doan_QLTV/Models/Database.cs
+++ QLTV_LTQL_N05/Doan_QLTV/Models/Database.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,25 @@ namespace Doan_QLTV
 {
     internal class Database
     {
+        public SqlConnection cn;
+        SqlDataAdapter da;
+        public Database(string srvname, string dbName)
+        {
+            string cnnstr = "Data source=" + srvname + "; Initial Catalog=" + dbName + "; Integrated Security=True";
+            cn = new SqlConnection(cnnstr);
+        }
+        public DataTable laydl(string sqlstr)
+        {
+            da = new SqlDataAdapter(sqlstr, cn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public void thucthi(SqlCommand sqlcmd)
+        {
+            cn.Open();
+            sqlcmd.ExecuteNonQuery();
+            cn.Close();
+        }
     }
 }
