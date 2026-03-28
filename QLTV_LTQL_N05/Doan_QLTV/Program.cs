a116871f19c811370d13a7fb1de47bee76933b9d
ChihaoDEV2025 <chihao.dev@gmail.com>
2026-03-28 23:36:22 +0700

    First commit

--- QLTV_LTQL_N05/Doan_QLTV/Program.cs
+++ QLTV_LTQL_N05/Doan_QLTV/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Doan_QLTV.Froms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Doan_QLTV
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMain());
+            Application.Run(new FrmTaiKhoan());
         }
     }
 }
